85b46b81cf10f625b9ea2d9932622056a2bcf956
hifromkevin <lam.kevin86@gmail.com>
2025-04-10 21:27:11 -0700

    Mocks News data

--- frontend/src/features/News/News.tsx
+++ frontend/src/features/News/News.tsx
@@ -1,7 +1,28 @@
+import Article from './modules/Article';
+
+import { useFetchNews } from '../../hooks/useFetchNews';
+
+import NewsIcon from '../../assets/svg/news.svg';
+
 const News = () => {
+  const { news } = useFetchNews();
+
   return (
-    <div className="newsCotainer">
-      <h1>News</h1>
+    <div className="newsContainer">
+      <h1 className="title">News</h1>
+
+      <div className="newsArticles">
+        {news.slice(0, 5).map((article, index) => (
+          <>
+            <img src={NewsIcon} alt="News Icon" className="newsIcon" />
+            <Article
+              key={index}
+              author={article.author}
+              title={article.title}
+            />
+          </>
+        ))}
+      </div>
     </div>
   );
 };
